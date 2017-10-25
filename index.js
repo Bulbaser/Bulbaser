372a688cc60e8959251b6203ac340f80b089955f
John Doe <johndoe@example.com>
2017-10-26 02:21:42 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const setResult=result => document.getElementById("result").innerHTML="Результат: ${result}";
+const addition = (a, b) => {
+	result = parseInt(a) + parseInt(b);
+	parseInt() setResult(`${a} + ${b} = ${result}`);
+}
+<a
+href="#"
+onclick="addition(document.getElementById('aValue').value,document.getElementById('bValue').value)"
+>Сложение</a>
+const addition = (a, b) => {
+	result = parseInt(a) - parseInt(b);
+	parseInt() setResult(`${a} - ${b} = ${result}`);
+}
+<a
+href="#"
+onclick="addition(document.getElementById('aValue').value,document.getElementById('bValue').value)"
+>Вычитание</a>
+const addition = (a, b) => {
+	result = parseInt(a) * parseInt(b);
+	parseInt() setResult(`${a} * ${b} = ${result}`);
+}
+<a
+href="#"
+onclick="addition(document.getElementById('aValue').value,document.getElementById('bValue').value)"
+>Умножение</a>
+const addition = (a, b) => {
+	result = parseInt(a) / parseInt(b);
+	parseInt() setResult(`${a} / ${b} = ${result}`);
+}
+<a
+href="#"
+onclick="addition(document.getElementById('aValue').value,document.getElementById('bValue').value)"
+>Деление</a>
+
+
+
+
+
